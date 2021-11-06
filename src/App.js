b53b8b632e8ce898da5c6d6f606522a829d892b6
Vedran Sustic <78386759+vedransustic@users.noreply.github.com>
2021-11-07 00:46:19 +0100

    frontend over... tidius work that I will remake in my own image...

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
-import './styles/App.css';
-import { Route, Switch, Redirect } from 'react-router-dom'
+import React, { useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-import MainNavigation from "./components/Navigation/MainNavigation";
-import Users from "./pages/Users";
-import NewPlace from "./pages/NewPlace"
-import UserPlaces from "./pages/UserPlaces";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
-      <>
-          <MainNavigation/>
-          <main>
-              <Switch>
-                  <Route exact path="/"><Users/></Route>
-                  <Route exact path="/:uid/places"><UserPlaces/></Route>
-                  <Route exact path="/places/new"><NewPlace/></Route>
-                  <Redirect to="/"/>
-              </Switch>
-          </main>
-
-      </>
-
-  )
-}
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>,
-  document.getElementById('root')
-);
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
